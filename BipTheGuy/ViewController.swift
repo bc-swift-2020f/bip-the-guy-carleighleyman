47ffb384085b802c3b905f2b373c6732e068d32b
Carleigh Leyman <leyman@bc.edu>
2020-09-20 13:32:48 -0400

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Carleigh Leyman on 9/20/20.
+//  Copyright © 2020 Carleigh Leyman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
